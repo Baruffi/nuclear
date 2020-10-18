4860f8048fe470a723fe7a4f0345863746fdd173
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2020-10-18 20:51:50 -0300

    adicionados multiplos sensores

--- api/app.py
+++ api/app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from redis import Redis
-from model.sensors import TemperatureSensor
+from model.sensors import SensorGroup, PowerSensor, PressureSensor, FlowSensor, TemperatureSensor
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-sensor = TemperatureSensor('CALDEIRA_1', 1000, 10)
+gep = PowerSensor('GEP', 450, 8.722)
+sg1fwp = PressureSensor('SG1FWP', 5.2, 0.03793)
+sg2fwp = PressureSensor('SG2FWP', 5.2, 0.04199)
+sg1fwf = FlowSensor('SG1FWF', 90, 25.34)
+sg2fwf = FlowSensor('SG2FWF', 120, 33.63)
+sg1sp = PressureSensor('SG1SP', 6.4, 0.02893)
+sg2sp = PressureSensor('SG2SP', 6.4, 0.02927)
+sg1sf = FlowSensor('SG1SF', 100, 35.04)
+sg2sf = FlowSensor('SG2SF', 140, 44.91)
+dtl1 = TemperatureSensor('DTL1', 177, 0.6807)
+dtl2 = TemperatureSensor('DTL2', 93, 0.6615)
+
+power_group = SensorGroup(gep)
+temperature_group = SensorGroup(dtl1, dtl2)
+pressure_group = SensorGroup(sg1fwp, sg2fwp, sg1sp, sg2sp)
+flow_group = SensorGroup(sg1fwf, sg2fwf, sg1sf, sg2sf)
 
 
 @app.route('/')
@@ -13,11 +28,30 @@ def hello():
     redis.incr('hits')
     return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
+
+@app.route('/power', defaults={'external': 0.0})
+@app.route('/power/<float:external>')
+def power(external):
+    return power_group.get_readings(GEP=external)
+
+
 @app.route('/temperature', defaults={'external': 0.0})
 @app.route('/temperature/<float:external>')
 def temperature(external):
-    return sensor.read(external)
+    return temperature_group.get_readings(DTL1=external, DTL2=external * 1.1)
 
 
-if __name__ == "__main__":
+@app.route('/pressure', defaults={'external': 0.0})
+@app.route('/pressure/<float:external>')
+def pressure(external):
+    return pressure_group.get_readings(SG1FWP=external, SG2FWP=external * 1.1, SG1SP=external * 1.2, SG2SP=external * 1.32)
+
+
+@app.route('/flow', defaults={'external': 0.0})
+@app.route('/flow/<float:external>')
+def flow(external):
+    return flow_group.get_readings(SG1FWF=external, SG2FWF=external * 1.1, SG1SF=external * 1.2, SG2SF=external * 1.32)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- api/model/sensors.py
+++ api/model/sensors.py
@@ -1,6 +1,26 @@
 import random
 
 
+class SensorGroup(object):
+    def __init__(self, *sensors):
+        self.sensors = [*sensors]
+
+    def get_sensor(self, id):
+        return next((sensor for sensor in self.sensors if sensor.id == id), None)
+
+    def get_readings(self, **externals):
+        return {'readings': [sensor.read(external) for id, external in externals.items() if (sensor := self.get_sensor(id))]}
+
+    def get_sensors(self):
+        return self.sensors
+
+    def set_sensors(self, sensors):
+        self.sensors = sensors
+
+    def add_sensors(self, *sensors):
+        self.sensors.extend(sensors)
+
+
 class Sensor(object):
     def __init__(self, id='0', mean=0, variance=0):
         self.id = id
@@ -13,10 +33,46 @@ class Sensor(object):
         }
 
 
+class PowerSensor(Sensor):
+    def read(self, external=0):
+        super_read = super().read()
+        value = round(random.gauss(self.mean, self.variance) + external, 5)
+
+        return {
+            **super_read,
+            'value': value,
+            'unit': 'MW'
+        }
+
+
+class PressureSensor(Sensor):
+    def read(self, external=0):
+        super_read = super().read()
+        value = round(random.gauss(self.mean, self.variance) + external, 5)
+
+        return {
+            **super_read,
+            'value': value,
+            'unit': 'MPa'
+        }
+
+
+class FlowSensor(Sensor):
+    def read(self, external=0):
+        super_read = super().read()
+        value = round(random.gauss(self.mean, self.variance) + external, 5)
+
+        return {
+            **super_read,
+            'value': value,
+            'unit': 't/h'
+        }
+
+
 class TemperatureSensor(Sensor):
     def read(self, external=0):
         super_read = super().read()
-        value = round(random.gauss(self.mean, self.variance) + external, 1)
+        value = round(random.gauss(self.mean, self.variance) + external, 5)
 
         return {
             **super_read,
