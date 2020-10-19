bda39499f1dc91bff41e258b036e5999e62ebb77
N Cipolli <49487964+ncipolli@users.noreply.github.com>
2020-10-19 00:21:11 -0300

    sensores separados

--- api/model/sensors.py
+++ api/model/sensors.py
@@ -1,26 +1,5 @@
 import random
 
-
-class SensorGroup(object):
-    def __init__(self, *sensors):
-        self.sensors = [*sensors]
-
-    def get_sensor(self, id):
-        return next((sensor for sensor in self.sensors if sensor.id == id), None)
-
-    def get_readings(self, **externals):
-        return {'readings': [sensor.read(external) for id, external in externals.items() if (sensor := self.get_sensor(id))]}
-
-    def get_sensors(self):
-        return self.sensors
-
-    def set_sensors(self, sensors):
-        self.sensors = sensors
-
-    def add_sensors(self, *sensors):
-        self.sensors.extend(sensors)
-
-
 class Sensor(object):
     def __init__(self, id='0', mean=0, variance=0):
         self.id = id
