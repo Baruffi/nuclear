4a1927931ac73a0d0fa60dd5bdf7caa65049eb1f
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2020-10-26 00:42:57 -0300

    otimizacao dos valores de sensores e rotinas e adicao de eventos de alarme no scadaBR

--- api/provider/sensor_provider.py
+++ api/provider/sensor_provider.py
@@ -2,16 +2,16 @@ from model.sensor import Sensor, PowerSensor, PressureSensor, FlowSensor, Temper
 
 sensors: list[Sensor] = [
     PowerSensor('GEP', 450, 8.722),
-    PressureSensor('SG1FWP', 5.2, 0.03793),
-    PressureSensor('SG2FWP', 5.2, 0.04199),
-    FlowSensor('SG1FWF', 120, 25.34),
-    FlowSensor('SG2FWF', 90, 33.63),
-    PressureSensor('SG1SP', 6.4, 0.02893),
-    PressureSensor('SG2SP', 6.4, 0.02927),
-    FlowSensor('SG1SF', 140, 35.04),
-    FlowSensor('SG2SF', 100, 44.91),
-    TemperatureSensor('DTL1', 177, 0.6807),
-    TemperatureSensor('DTL2', 93, 0.6615)
+    PressureSensor('SG1FWP', 12.22, 0.03793),
+    PressureSensor('SG2FWP', 12.32, 0.04199),
+    FlowSensor('SG1FWF', 29000, 25.34),
+    FlowSensor('SG2FWF', 30000, 33.63),
+    PressureSensor('SG1SP', 6.35, 0.02893),
+    PressureSensor('SG2SP', 6.45, 0.02927),
+    FlowSensor('SG1SF', 2900, 35.04),
+    FlowSensor('SG2SF', 3000, 44.91),
+    TemperatureSensor('DTL1', 210, 0.6807),
+    TemperatureSensor('DTL2', 200, 0.6615)
 ]
 
 
